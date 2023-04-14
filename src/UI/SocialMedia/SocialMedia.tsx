6c73fbc3cdf299470aeee13033527995d5376145
Dunissimo <SuperDen214@yandex.ru>
2023-04-14 16:56:25 +0400

    make social media icons component

---
+++ src/UI/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,65 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./SocialMedia.module.scss";
+
+import yotube from "../../assets/icons/YouTube.svg";
+import twitter from "../../assets/icons/Twitter.svg";
+import facebook from "../../assets/icons/Facebook.svg";
+import telegram from "../../assets/icons/telegram.svg";
+import instagram from "../../assets/icons/Instagram.svg";
+import linkedIn from "../../assets/icons/Linked-In.svg";
+import clsx from "clsx";
+
+const links = {
+  youtube: {
+    link: "https://www.youtube.com/",
+    img: yotube,
+  },
+  twitter: {
+    link: "https://twitter.com/",
+    img: twitter,
+  },
+  facebook: {
+    link: "https://www.facebook.com",
+    img: facebook,
+  },
+  telegram: {
+    link: "https://web.telegram.org/",
+    img: telegram,
+  },
+  instagram: {
+    link: "https://instagram.com/",
+    img: instagram,
+  },
+  linkedIn: {
+    link: "https://www.linkedin.com/",
+    img: linkedIn,
+  },
+};
+
+const images = {};
+
+interface IProps {
+  social:
+    | "youtube"
+    | "twitter"
+    | "facebook"
+    | "telegram"
+    | "instagram"
+    | "linkedIn";
+  className: string;
+}
+
+const SocialMedia: FC<IProps> = ({ social, className }) => {
+  return (
+    <Link
+      to={links[social].link}
+      className={clsx(styles.social, styles[className])}
+    >
+      <img src={links[social].img} alt={`${social} icon`} />
+    </Link>
+  );
+};
+
+export default SocialMedia;
