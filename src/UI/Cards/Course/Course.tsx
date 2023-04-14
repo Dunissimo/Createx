5c9b79d33ce4cd0aca28b0ba32d0e00dda3be878
Dunissimo <SuperDen214@yandex.ru>
2023-04-14 18:31:33 +0400

    make course ui component

--- src/UI/Cards/Course/Course.tsx
+++ src/UI/Cards/Course/Course.tsx
@@ -1,19 +1,39 @@
 import { FC } from "react";
 
-const Course: FC = () => {
+import styles from "./Course.module.scss";
+
+import notFound from "../../../assets/imgNotFound.png";
+import clsx from "clsx";
+
+interface IProps {
+  type: "Marketing" | "Management" | "Recruting" | "Design" | "Development";
+  header: string;
+  price: string | number;
+  author: string;
+}
+
+const CourseUI: FC<IProps> = ({ type, header, author, price }) => {
+  console.log(type.toLowerCase());
+
   return (
-    <div>
-      <img src="#" alt="" />
-      <div className="">
-        <span>TYPE</span>
-        <h2>HEADER</h2>
-        <div className="">
-          <span>PRICE</span>
-          <span>| by AUTHOR</span>
+    <div className={styles.card}>
+      <img src={notFound} alt="" />
+      <div className={styles.cardInfo}>
+        <span
+          className={clsx(
+            styles[`card-${type.toLowerCase()}`],
+            styles.cardType
+          )}
+        >
+          {type}
+        </span>
+        <h2>{header}</h2>
+        <div className={styles.cardPrice}>
+          <span>${price}</span>|<span>by {author}</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default Course;
+export default CourseUI;
