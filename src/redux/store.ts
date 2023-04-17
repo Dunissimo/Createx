59485b5cf759a5503d02b8b4ca3895bc146a8fe6
Dunissimo <SuperDen214@yandex.ru>
2023-04-17 18:41:28 +0400

    make eventsList component

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { coursesReducer } from "./slices/coursesSlice";
+import { eventsReducer } from "./slices/eventsSlice";
 
 const rootReducer = combineReducers({
   courses: coursesReducer,
+  events: eventsReducer,
 });
 
 export const store = configureStore({
